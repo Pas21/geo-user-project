0b605b2e8aa028f9b893931435125afd82724d46
Pasquale Forgione <pasqualeforgione21@gmail.com>
2017-12-29 00:23:57 +0100

    Lanciare test con versione JDK7 e 8 per vedere se il problema di Travis CI è casuato dalla versione della JDK

--- ServerGeoUserGradle/src/main/java/server/backend/GestoreDatiPersistenti.java
+++ ServerGeoUserGradle/src/main/java/server/backend/GestoreDatiPersistenti.java
@@ -4,8 +4,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
-import javax.persistence.PersistenceException;
-
 import java.util.TreeMap;
 
 import org.hibernate.Session;
@@ -21,51 +19,51 @@ public class GestoreDatiPersistenti {
 	//Pattern Singleton
 	private GestoreDatiPersistenti(){
 		try {
-	         this.factory = new Configuration().configure().buildSessionFactory();
-	      } catch (Throwable ex) { 
-	         System.err.println("Failed to create sessionFactory object." + ex);
-	         throw new ExceptionInInitializerError(ex); 
-	      }
+			this.factory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) { 
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex); 
+		}
 	}
-	
+
 	//Costruttore pubblico
 	public static synchronized GestoreDatiPersistenti getInstance(){
 		if(instance==null)
 			instance=new GestoreDatiPersistenti();
 		return instance;
 	}
-	
+
 	//Per JUnit
 	public synchronized SessionFactory getFactory(){
 		return factory;
 	}
 
-	
+
 	//Metodo per ottenere dal database tutti gli utenti
 	public TreeMap<String,Utente> getUtenti(){
-	      Session session = this.factory.openSession();
-	      Transaction tx = null;
-	      TreeMap<String,Utente> utenti;
-	      
-	      try{
-	         tx = session.beginTransaction();
-	         utenti = new TreeMap<String,Utente>();
-	         List<?> listaUtenti = session.createQuery("FROM Utente").list(); 		// Query su classe java e non su tabella
-	         for (Iterator<?> iterator = listaUtenti.iterator(); iterator.hasNext();){
-	            Utente utente = (Utente) iterator.next(); 
-	            utenti.put(utente.getUsername(), utente);
-	            //System.out.println(utente.toString());
-	         }
-	         tx.commit();
-	      }catch (PersistenceException e) {
-	         if (tx!=null) tx.rollback();
-	         utenti = null;
-	         e.printStackTrace();
-	      }finally{
-	         session.close();
-	      }
-	      return utenti;
-	   }
+		Session session = this.factory.openSession();
+		Transaction tx = null;
+		TreeMap<String,Utente> utenti;
+
+		try{
+			tx = session.beginTransaction();
+			utenti = new TreeMap<String,Utente>();
+			List<?> listaUtenti = session.createQuery("FROM Utente").list(); 		// Query su classe java e non su tabella
+			for (Iterator<?> iterator = listaUtenti.iterator(); iterator.hasNext();){
+				Utente utente = (Utente) iterator.next(); 
+				utenti.put(utente.getUsername(), utente);
+				//System.out.println(utente.toString());
+			}
+			tx.commit();
+		}catch (Exception e) {
+			if (tx!=null) tx.rollback();
+			utenti = null;
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+		return utenti;
+	}
 
 	//Metodo per ottenere dal database tutte le posizioni degli utenti
 	public TreeMap<IdPosizione,Posizione> getPosizioni(){
@@ -80,10 +78,10 @@ public class GestoreDatiPersistenti {
 				Posizione posizione = (Posizione) iterator.next(); 
 				IdPosizione idPosizione = new IdPosizione(posizione.getIdPosizione().getTimestamp(), posizione.getIdPosizione().getLatitudine(), posizione.getIdPosizione().getLongitudine());
 				posizioni.put(idPosizione,posizione);
-	            //System.out.println(posizione.toString());
+				//System.out.println(posizione.toString());
 			}
 			tx.commit();
-		}catch (PersistenceException e) {
+		}catch (Exception e) {
 			if (tx!=null) tx.rollback();
 			e.printStackTrace(); 
 		}finally{
@@ -91,87 +89,87 @@ public class GestoreDatiPersistenti {
 		}
 		return posizioni;
 	}
-	
-	
+
+
 	// Metodo per aggiungere un utente al database
 	public boolean addUtente(Utente utente){
 		Session session = this.factory.openSession();
-	    Transaction tx = null;
-	    boolean addok = true;
-	    try{
-	       tx = session.beginTransaction();
-	       session.save(utente); 
-	       tx.commit();
-	    }catch (PersistenceException e) {
-	       if (tx!=null) tx.rollback();
-	       e.printStackTrace(); 
-	       addok=false;
-	    }finally{
-	       session.close(); 
-	    }
-	    return addok;
+		Transaction tx = null;
+		boolean addok = true;
+		try{
+			tx = session.beginTransaction();
+			session.save(utente); 
+			tx.commit();
+		}catch (Exception e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace(); 
+			addok=false;
+		}finally{
+			session.close(); 
+		}
+		return addok;
 	}
-	
+
 	// Metodo per cancellare un utente dal database 
 	public boolean removeUtente(Utente utente){
 		Session session = this.factory.openSession();
-	    Transaction tx = null;
-	    boolean removeok=true;
-	    
-	    try{
-	       tx = session.beginTransaction();
-	       session.delete(utente); 
-	       tx.commit();
-	    }catch (PersistenceException e) {
-	       if (tx!=null) tx.rollback();
-	       e.printStackTrace(); 
-	       removeok=false;
-	    }finally{
-	       session.close();	       
-	    }
-	    return removeok;
+		Transaction tx = null;
+		boolean removeok=true;
+
+		try{
+			tx = session.beginTransaction();
+			session.delete(utente); 
+			tx.commit();
+		}catch (Exception e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace(); 
+			removeok=false;
+		}finally{
+			session.close();	       
+		}
+		return removeok;
 	}
-	
+
 	// Metodo per aggiungere una posizione al database 
 	public boolean addPosizione(Posizione posizione){
 		Session session = this.factory.openSession();
-	    Transaction tx = null;
-	    boolean addok = true;
-	    
-	    try{
-	       tx = session.beginTransaction();
-	       session.save(posizione); 
-	       tx.commit();
-	    }catch (PersistenceException e) {
-	       if (tx!=null) tx.rollback();
-	       e.printStackTrace(); 
-	       addok=false;
-	    }finally{
-	       session.close(); 
-	    }
-	    return addok;
+		Transaction tx = null;
+		boolean addok = true;
+
+		try{
+			tx = session.beginTransaction();
+			session.save(posizione); 
+			tx.commit();
+		}catch (Exception e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace(); 
+			addok=false;
+		}finally{
+			session.close(); 
+		}
+		return addok;
 	}
-	
+
 	// Metodo per cancellare una posizione dal database 
 	public boolean removePosizione(Posizione posizione){
 		Session session = this.factory.openSession();
-	    Transaction tx = null;
-	    boolean removeok=true;
-	    
-	    try{
-	       tx = session.beginTransaction();
-	       session.delete(posizione); 
-	       tx.commit();
-	    }catch (PersistenceException e) {
-	       if (tx!=null) tx.rollback();
-	       e.printStackTrace(); 
-	       removeok=false;
-	    }finally{
-	       session.close(); 
-	    }
-	    return removeok;
+		Transaction tx = null;
+		boolean removeok=true;
+
+		try{
+			tx = session.beginTransaction();
+			session.delete(posizione); 
+			tx.commit();
+		}catch (Exception e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace(); 
+			removeok=false;
+		}finally{
+			session.close(); 
+		}
+		return removeok;
 	}
-	
+
 	//Metodo per rimuovere tutte le posizioni di un utente
 	public boolean removePosizioniUtente(Utente utente) {
 		TreeMap<IdPosizione, Posizione> posizioni = this.getPosizioni();		
@@ -179,7 +177,7 @@ public class GestoreDatiPersistenti {
 		Transaction tx = null;
 		boolean removeok =  true;
 		Posizione posizione = null;
-		
+
 		try{
 			//Si puo' fare anche con createNativeQuery!
 			for(Entry<IdPosizione, Posizione> id : posizioni.entrySet()) {
@@ -188,21 +186,21 @@ public class GestoreDatiPersistenti {
 				if(posizione.getUtente().getUsername().equals(utente.getUsername())) {
 					tx = session.beginTransaction();
 					session.delete(posizione); 
-				    tx.commit();			
+					tx.commit();			
 				}
 			}
-		}catch (PersistenceException e) {
+		}catch (Exception e) {
 			if (tx!=null) tx.rollback();
-		    e.printStackTrace(); 
-		    removeok=false;
+			e.printStackTrace(); 
+			removeok=false;
 		}finally{
 			session.close(); 
 		}
 		return removeok;
 	}
 
-	
-	 
+
+
 	public SessionFactory factory; 	
 	private static GestoreDatiPersistenti instance;
 }
