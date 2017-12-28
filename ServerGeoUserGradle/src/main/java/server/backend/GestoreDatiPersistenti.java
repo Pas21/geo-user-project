15dd094554c9146788cfb567df147bac718f1f19
Pasquale Forgione <pasqualeforgione21@gmail.com>
2017-12-28 20:45:39 +0100

    Ultima versione non funzionante su Travis CI #10

--- ServerGeoUserGradle/src/main/java/server/backend/GestoreDatiPersistenti.java
+++ ServerGeoUserGradle/src/main/java/server/backend/GestoreDatiPersistenti.java
@@ -98,7 +98,7 @@ public class GestoreDatiPersistenti {
 	    boolean addok = true;
 	    try{
 	       tx = session.beginTransaction();
-	       session.persist(utente); 
+	       session.save(utente); 
 	       tx.commit();
 	    }catch (PersistenceException e) {
 	       if (tx!=null) tx.rollback();
