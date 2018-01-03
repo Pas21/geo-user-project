8f297efbe4bb130d040ff1da78c01fcde72b4a1f
Pasquale Forgione <pasqualeforgione21@gmail.com>
2018-01-04 00:27:12 +0100

    Correzione caratteri con accento

--- ServerGeoUserGradle/src/test/java/server/web/resources/json/LastPositionAllUsersJSONTest.java
+++ ServerGeoUserGradle/src/test/java/server/web/resources/json/LastPositionAllUsersJSONTest.java
@@ -92,8 +92,8 @@ public class LastPositionAllUsersJSONTest {
 		System.out.println(mapLastPositionAllUsers);
 		
 		assertTrue("La mappa dovrebbe contenere 3 elementi", mapLastPositionAllUsers.size()==3);
-		assertTrue("L'ultima posizione dell'utente "+u1.getUsername()+" non è quella prevista", mapLastPositionAllUsers.containsKey(u1.getUsername()) && mapLastPositionAllUsers.get(u1.getUsername()).equals(pos1c));
-		assertTrue("L'ultima posizione dell'utente "+u2.getUsername()+" non è quella prevista", mapLastPositionAllUsers.containsKey(u2.getUsername()) && mapLastPositionAllUsers.get(u2.getUsername()).equals(pos2b));
-		assertTrue("L'ultima posizione dell'utente "+u3.getUsername()+" non è quella prevista", mapLastPositionAllUsers.containsKey(u3.getUsername()) && mapLastPositionAllUsers.get(u3.getUsername()).equals(pos3a));	
+		assertTrue("L'ultima posizione dell'utente "+u1.getUsername()+" non e' quella prevista", mapLastPositionAllUsers.containsKey(u1.getUsername()) && mapLastPositionAllUsers.get(u1.getUsername()).equals(pos1c));
+		assertTrue("L'ultima posizione dell'utente "+u2.getUsername()+" non e' quella prevista", mapLastPositionAllUsers.containsKey(u2.getUsername()) && mapLastPositionAllUsers.get(u2.getUsername()).equals(pos2b));
+		assertTrue("L'ultima posizione dell'utente "+u3.getUsername()+" non e' quella prevista", mapLastPositionAllUsers.containsKey(u3.getUsername()) && mapLastPositionAllUsers.get(u3.getUsername()).equals(pos3a));	
 	}
 }
